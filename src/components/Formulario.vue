245c9dd4755e4e9665a02734e6328fadecda3a16
Oneal <oneal152@gmail.com>
2024-03-10 17:49:24 +0100

    montando formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center">Seguimiento pacientes</h2>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
