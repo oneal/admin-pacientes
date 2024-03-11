d92a69e5ea351f133f3c7dbec8888cff4e5cb048
Oneal <oneal152@gmail.com>
2024-03-11 18:26:03 +0100

    finalizado proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,106 @@
 <script setup>
+import {ref, reactive, onMounted, watch} from "vue";
+import {uid} from 'uid'
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
+import Paciente from "./components/Paciente.vue";
+
+const pacientes = ref([])
+
+const paciente = reactive({
+  id: null,
+  nombre:'',
+  propietario:'',
+  email:'',
+  alta:'',
+  sintomas:''
+})
+
+watch(pacientes, () => {
+  guardarLocalStorage()
+}, {
+  deep: true
+})
+
+onMounted(() => {
+  const pacientesStorage = localStorage.getItem('itemsPacientes');
+  if(pacientesStorage) {
+    pacientes.value = JSON.parse(pacientesStorage)
+  }
+})
+
+const guardarLocalStorage = () => {
+  localStorage.setItem('itemsPacientes', JSON.stringify(pacientes.value))
+}
+
+const guardarPaciente = () => {
+  if(paciente.id) {
+    const index = pacientes.value.findIndex(p => p.id === paciente.id)
+    pacientes.value[index] = {...paciente}
+  } else {
+    pacientes.value.push({
+      ...paciente,
+      id: uid()
+    })
+  }
+
+  Object.assign(paciente, {
+    nombre:'',
+    propietario:'',
+    email:'',
+    alta:'',
+    sintomas:''
+  })
+}
+
+const editarPaciente = (id) => {
+  const index = pacientes.value.findIndex(p => p.id === id)
+
+  Object.assign(paciente, {
+    nombre:pacientes.value[index].nombre,
+    propietario:pacientes.value[index].propietario,
+    email:pacientes.value[index].email,
+    alta:pacientes.value[index].alta,
+    sintomas:pacientes.value[index].sintomas,
+    id:pacientes.value[index].id,
+  })
+}
+
+const eliminarPaciente = (id) => {
+  pacientes.value = pacientes.value.filter(paciente => paciente.id !== id)
+}
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
     <Header/>
     <div class="mt-12 md:flex">
-      <Formulario/>
+      <Formulario
+          v-model:nombre="paciente.nombre"
+          v-model:propietario="paciente.propietario"
+          v-model:email="paciente.email"
+          v-model:alta="paciente.alta"
+          v-model:sintomas="paciente.sintomas"
+          :id="paciente.id"
+          @guardar-paciente="guardarPaciente"
+      />
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
+
+        <div v-if="pacientes.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Información de
+            <span class="text-indigo-600 font-bold">pacientes</span>
+          </p>
+          <Paciente
+              v-for="paciente in pacientes"
+              :paciente="paciente"
+              @editar-paciente="editarPaciente"
+              @eliminar-paciente="eliminarPaciente"
+          />
+        </div>
+        <p v-else class="mt-10 text-2xl text-center mt-20">No hay pacientes</p>
+      </div>
     </div>
   </div>
 </template>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,10 +1,145 @@
 <script setup>
+import {computed, reactive} from 'vue'
+import Alerta from "./Alerta.vue";
+
+const alerta = reactive({
+  tipo:'',
+  mensaje:''
+})
+
+const emit = defineEmits([
+  'update:nombre',
+  'update:propietario',
+  'update:email',
+  'update:alta',
+  'update:sintomas',
+  'guardar-paciente'
+])
+
+const props = defineProps({
+  nombre: {
+    type:String,
+    required:true
+  },
+  propietario: {
+    type:String,
+    required:true
+  },
+  email: {
+    type:String,
+    required:true
+  },
+  alta: {
+    type:String,
+    required:true
+  },
+  sintomas: {
+    type:String,
+    required:true
+  },
+  id: {
+    type:[String, null],
+    required:true
+  }
+})
+
+const validar = (e) => {
+  if(Object.values(props).includes('')) {
+    alerta.mensaje='Todos los campos son obligatorios'
+    alerta.tipo='error'
+    setTimeout(() => {
+      Object.assign(alerta, {
+        tipo:'',
+        mensaje:''
+      })
+    },3000)
+    return
+  }
+  emit('guardar-paciente')
+  alerta.mensaje='Paciente agregado'
+  alerta.tipo='exito'
+
+  setTimeout(() => {
+    Object.assign(alerta, {
+      tipo:'',
+      mensaje:''
+    })
+  },3000)
+}
+
+const editando = computed(()=>{
+  return props.id
+})
 
 </script>
 
 <template>
   <div class="md:w-1/2">
     <h2 class="font-black text-3xl text-center">Seguimiento pacientes</h2>
+    <p class="text-lg mt-5 text-center mb-10">
+      Añade paciente y
+      <span class="text-indigo-600 font-bold">Administralos</span>
+    </p>
+    <Alerta v-if="alerta.mensaje" :alerta="alerta"/>
+    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" @submit.prevent="validar">
+      <div class="mb-5">
+        <label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre mascota</label>
+        <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            :value="nombre"
+            @input="$emit('update:nombre', $event.target.value)"
+        />
+      </div>
+      <div class="mb-5">
+        <label for="propietario" class="block text-gray-700 uppercase font-bold">Nombre propietario</label>
+        <input
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propietario"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            :value="propietario"
+            @input="$emit('update:propietario', $event.target.value)"
+        />
+      </div>
+      <div class="mb-5">
+        <label for="email" class="block text-gray-700 uppercase font-bold">Email propietario</label>
+        <input
+            id="email"
+            type="email"
+            placeholder="Email del propietario"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            :value="email"
+            @input="$emit('update:email', $event.target.value)"
+        />
+      </div>
+      <div class="mb-5">
+        <label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
+        <input
+            id="alta"
+            type="date"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            :value="alta"
+            @input="$emit('update:alta', $event.target.value)"
+        />
+      </div>
+      <div class="mb-5">
+        <label for="sintomas" class="block text-gray-700 uppercase font-bold">Sintomas</label>
+        <textarea
+            id="sintomas"
+            placeholder="Describe los sintomas del paciente"
+            class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+            :value="sintomas"
+            @input="$emit('update:sintomas', $event.target.value)"
+        />
+      </div>
+      <input type="submit"
+             class="bg-indigo-600 w-full p-3 text-white uppercase
+             font-bold hover:bg-indigo-600 cursor-pointer transition-colors"
+             :value="[editando ? 'Guardar paciente' : 'Registrar paciente']"/>
+    </form>
   </div>
 </template>
 
